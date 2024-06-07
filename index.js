c025365b3c34c77c198c9c6500e5e210a9ac6fc9
Daniel0602 <danieldealesandro06@gmail.com>
2024-06-07 20:52:50 -0300

    dyjhs

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Servir archivos estáticos desde el directorio 'public'
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Ruta para la página principal
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Servir archivos estáticos desde la raíz del proyecto
+app.use(express.static(path.join(__dirname)));
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado en el puerto ${PORT}`);
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
